3825f6f5a7c74573e16f88d1f05931b5e60a0e5e
Yoon Chang <yoonsjchang@gmail.com>
2020-02-03 22:58:10 -0500

    Update table initializer

--- setup.js
+++ setup.js
@@ -6,19 +6,36 @@ const userTableQuery = `CREATE TABLE IF NOT EXISTS \`users\` (
     \`email\` varchar(255) NOT NULL,
     \`firstname\` varchar(255) DEFAULT NULL,
     \`lastname\` varchar(255) DEFAULT NULL,
+    \`password\` varchar(255) DEFAULT NULL,
     PRIMARY KEY (\`userId\`),
     UNIQUE KEY \`email_UNIQUE\` (\`email\`),
     UNIQUE KEY \`username_UNIQUE\` (\`username\`),
     KEY \`ix_username\` (\`username\`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+  ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+  
 
   const eventTableQuery = `CREATE TABLE IF NOT EXISTS \`events\` (
     \`eventId\` int(11) NOT NULL AUTO_INCREMENT,
     \`name\` varchar(45) NOT NULL,
-    \`start\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    \`end\` datetime DEFAULT NULL,
-    PRIMARY KEY (\`eventId\`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`;
+    \`start\` timestamp NULL DEFAULT NULL,
+    \`end\` timestamp NULL DEFAULT NULL,
+    \`createdBy\` int(11) DEFAULT NULL,
+    \`createdOn\` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (\`eventId\`),
+    KEY \`userId_idx\` (\`createdBy\`),
+    CONSTRAINT \`createdBy\` FOREIGN KEY (\`createdBy\`) REFERENCES \`users\` (\`userId\`) ON DELETE CASCADE
+  ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+  
+
+  const eventMapTableQuery = `CREATE TABLE IF NOT EXISTS \`eventMap\` (
+    \`userId\` int(11) DEFAULT NULL,
+    \`eventId\` int(11) DEFAULT NULL,
+    KEY \`userId_idx\` (\`userId\`),
+    KEY \`eventId_idx\` (\`eventId\`),
+    CONSTRAINT \`eventId\` FOREIGN KEY (\`eventId\`) REFERENCES \`events\` (\`eventId\`) ON DELETE CASCADE,
+    CONSTRAINT \`userId\` FOREIGN KEY (\`userId\`) REFERENCES \`users\` (\`userId\`) ON DELETE CASCADE
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci`;
+  
 
 module.exports =  {
     dbSetup: function (){
@@ -36,6 +53,14 @@ module.exports =  {
                 console.log(err);
             }
             else console.log("Events table created.");
+        })
+
+        conn.query(eventMapTableQuery, (err, data) => {
+            if(err) {
+                console.log("Failed to set up eventMap table.");
+                console.log(err);
+            }
+            else console.log("Event map table created.")
         })
     }
 }
